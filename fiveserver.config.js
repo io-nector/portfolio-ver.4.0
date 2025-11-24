858fea09c2f5641b52cc1246e3783eb23fa862de
ionector <151391198+io-nector@users.noreply.github.com>
2025-11-23 21:17:48 -0800

    updated mobile site

---
+++ fiveserver.config.js
@@ -0,0 +1,6 @@
+// fiveserver.config.js
+module.exports = {
+    host: '0.0.0.0', // default: '0.0.0.0' (binds to all available network interfaces)
+    remoteLogs: 'magenta', // true | false | Color
+    useLocalIp: true, // optional: opens browser with your local IP
+}
